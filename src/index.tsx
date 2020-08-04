6fc0cbbdedf2fa4907507c0b90063ba42f7424c2
Victor Chirino <vicchirino@gmail.com>
2020-08-04 14:58:07 -0300

    remove font

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import './fonts/Autumn-Happiness.ttf';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
